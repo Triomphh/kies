07943ae5d4ba386e8831c07cd37fbe2388fd90be
Triomph <worker13prime@gmail.com>
2025-04-01 17:36:57 +0200

    Ajout table Question

---
+++ src/main/java/jpa/pojo/Question.java
@@ -0,0 +1,41 @@
+package jpa.pojo;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long questionId;
+
+    @Column(name = "turn_number")
+    private Integer turnNumber;
+
+    @Column(name = "text")
+    private String text;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "answer")
+    private String answer;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "round", referencedColumnName = "round_id")
+    private Round round;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "asker", referencedColumnName = "player_id")
+    private Player asker;
+    
+    
+    // Getters et Setters
+    // ...
+}
