50154bee91de2b431464ed49c2fa94113f4d93f0
fconderine <conderine.florent@etud.univ-pau.fr>
2025-04-01 03:46:40 +0200

    Ajout de la classe main afin de réaliser des tests avec une bdd

---
+++ src/main/java/JPA/Main.java
@@ -0,0 +1,31 @@
+package JPA;
+
+import JPA.POJO.Player;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.*;
+
+public class Main {
+    public static void main(String[ ] args)
+    {
+        // Création de l'EntityManagerFactory
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("kies");
+        // Récupération de l'EntityManager
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        // Création d'une entité
+        Player player = new Player();
+        player.setPseudo("Martin");
+        System.out.println(player.getPseudo());
+        // Début de la transaction
+        entityManager.getTransaction().begin();
+        // Persistance de l'entité dans la base de données
+        entityManager.persist(player);
+        // Validation de la transaction
+        entityManager.getTransaction().commit();
+        // Fermeture de l'EntityManager
+        entityManager.close();
+        // Fermeture de l'EntityManagerFactory
+        entityManagerFactory.close();
+
+    }
+}
