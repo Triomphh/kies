b786e863c2770634faff1a55479f1387bc2cf488
Triomph <worker13prime@gmail.com>
2025-04-01 17:34:08 +0200

    Ajout table Round

---
+++ src/main/java/jpa/pojo/Round.java
@@ -0,0 +1,33 @@
+package jpa.pojo;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "rounds")
+public class Round {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "round_id")
+    private Long roundId;
+
+    @Column(name = "round_number")
+    private Integer roundNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "winner", referencedColumnName = "player_id")
+    private Player winner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game", referencedColumnName = "game_id")
+    private Game game;
+
+
+    // Getters et Setters
+    // ...
+}
