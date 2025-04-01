d85649d9dff268ee972223d109a3b1607dde6c90
Triomph <worker13prime@gmail.com>
2025-04-01 17:40:24 +0200

    Ajout table Category

---
+++ src/main/java/jpa/pojo/Category.java
@@ -0,0 +1,25 @@
+package jpa.pojo;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator", referencedColumnName = "account_id")
+    private Account creator;
+    
+    
+    // Getters et Setters
+    // ...
+}
