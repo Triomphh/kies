5d42d7d78255af620f45b3ee1807edf61def85be
Triomph <worker13prime@gmail.com>
2025-04-18 14:45:54 +0200

    Ajout methode findByNickname

--- backend/src/main/java/dev/triomph/kies/DAO/PlayerDAO.java
+++ backend/src/main/java/dev/triomph/kies/DAO/PlayerDAO.java
@@ -1,8 +1,12 @@
 package dev.triomph.kies.DAO;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.triomph.kies.pojo.Player;
 
 
-public interface PlayerDAO extends JpaRepository<Player, Long> { }
+public interface PlayerDAO extends JpaRepository<Player, Long> {
+    Optional<Player> findByNickname(String nickname);
+}
