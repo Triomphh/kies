8803ec7eb0a7cec85bde1202324d5024da003a29
Triomph <worker13prime@gmail.com>
2025-04-18 15:32:54 +0200

    Remove le POJO de test Fake

--- backend/src/main/java/dev/triomph/kies/DAO/FakeDAO.java
+++
@@ -1,8 +0,0 @@
-package dev.triomph.kies.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.triomph.kies.pojo.Fake;
-
-
-public interface FakeDAO extends JpaRepository<Fake, Long> { }
