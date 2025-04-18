ae6da8ad611ff8151843a14e19da0374d37574bb
Triomph <worker13prime@gmail.com>
2025-04-18 14:50:38 +0200

    Remplace test Fake data avec Player+Account

--- backend/src/main/java/dev/triomph/kies/Application.java
+++ backend/src/main/java/dev/triomph/kies/Application.java
@@ -1,42 +1,45 @@
 package dev.triomph.kies;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import dev.triomph.kies.DAO.FakeDAO;
-import dev.triomph.kies.pojo.Fake;
-
+import dev.triomph.kies.pojo.Player;
+import dev.triomph.kies.pojo.Account;
+import dev.triomph.kies.pojo.Gender;
+import dev.triomph.kies.service.PlayerService;
+import dev.triomph.kies.service.AccountService;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
 public class Application {
+
 	@Autowired
-	private FakeDAO fakeDAO;
+	private PlayerService playerService;
+	
+	@Autowired
+	private AccountService accountService;
 
 	@GetMapping("/")
 	public String home() {
-		return "Number: " + fakeDAO.findAll().size();
+		return "Salut";
 	}
 	
 	@EventListener(ApplicationReadyEvent.class)
 	public void runAfterStartup() {
-		List<Fake> allFakes = this.fakeDAO.findAll();
-		System.out.println("Number of fakes:" + allFakes.size());
-
-		Fake newFake = new Fake();
-		newFake.setFirstName("John");
-		newFake.setLastName("Doe");
-		this.fakeDAO.save(newFake);
+		Optional<Player> existingPlayer = playerService.getPlayerByNickname("Jojo");
 		
-		allFakes = this.fakeDAO.findAll();
-		System.out.println("Number of fakes:" + allFakes.size());
+		if (existingPlayer.isEmpty()) {
+			Player player = playerService.createPlayer("Jojo");
+			Account account = accountService.createAccount(player, "mdp", 18, Gender.MALE);
+		} else {
+			System.out.println("Existe déjà");
+		}
 	}
 
 	public static void main(String[] args) {
