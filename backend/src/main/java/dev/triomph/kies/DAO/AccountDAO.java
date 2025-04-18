39a8497c5ae1e91eaebc6be56fbb4f295c416aeb
Triomph <worker13prime@gmail.com>
2025-04-18 14:46:51 +0200

    Ajout methode findByPlayerId

--- backend/src/main/java/dev/triomph/kies/DAO/AccountDAO.java
+++ backend/src/main/java/dev/triomph/kies/DAO/AccountDAO.java
@@ -1,8 +1,12 @@
 package dev.triomph.kies.DAO;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.triomph.kies.pojo.Account;
 
 
-public interface AccountDAO extends JpaRepository<Account, Long> { }
+public interface AccountDAO extends JpaRepository<Account, Long> {
+    Optional<Account> findByPlayerPlayerId(Long playerId);
+}
