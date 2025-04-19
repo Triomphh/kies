b07586c47f50b2c01c606f9ace0521a8c9634875
Triomph <worker13prime@gmail.com>
2025-04-19 11:00:17 +0200

    Ajout methode findByName CategoryDAO

--- backend/src/main/java/dev/triomph/kies/DAO/CategoryDAO.java
+++ backend/src/main/java/dev/triomph/kies/DAO/CategoryDAO.java
@@ -1,8 +1,11 @@
 package dev.triomph.kies.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 import dev.triomph.kies.pojo.Category;
 
 
-public interface CategoryDAO extends JpaRepository<Category, Long> { }
+public interface CategoryDAO extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
