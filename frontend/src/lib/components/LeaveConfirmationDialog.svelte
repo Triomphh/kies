73f345910c41f5f71ab2f0f70683761ac2dfb6a7
Triomph <worker13prime@gmail.com>
2025-04-26 10:39:50 +0200

    Change le blur des popups

--- frontend/src/lib/components/LeaveConfirmationDialog.svelte
+++ frontend/src/lib/components/LeaveConfirmationDialog.svelte
@@ -37,11 +37,12 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
     display: flex;
     justify-content: center;
     align-items: center;
     z-index: 1000;
+    backdrop-filter: blur(2px);
+    -webkit-backdrop-filter: blur(2px);
   }
 
   .main_message_span {
